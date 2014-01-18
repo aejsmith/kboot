06ab7563ce2186d3bfb3ac0a193e819529a9d6fe
Alex Smith <alex@alex-smith.me.uk>
2014-01-18 13:41:20 +0000

    Add loader build script, initial BIOS startup code and CD boot sector
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

---
+++ source/loader/platform/bios/platform.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2014 Alex Smith
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * @file
+ * @brief		BIOS platform startup code.
+ */
+
+#include <loader.h>
+
+/** Main function of the BIOS loader. */
+void platform_init(void) {
+	while(true) {}
+}
+
+/** Reboot the system. */
+void platform_reboot(void) {
+	while(true) {}
+}
