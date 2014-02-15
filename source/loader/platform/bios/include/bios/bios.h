0267b95746d124faa8b75efd0954a10831b9ad16
Alex Smith <alex@alex-smith.me.uk>
2014-02-15 10:51:22 +0000

    BIOS: Add function to call BIOS interrupts
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- source/loader/platform/bios/include/bios/bios.h
+++ source/loader/platform/bios/include/bios/bios.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Alex Smith
+ * Copyright (C) 2010-2014 Alex Smith
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -22,11 +22,62 @@
 #ifndef __BIOS_BIOS_H
 #define __BIOS_BIOS_H
 
-#ifndef __ASM__
+#include <lib/string.h>
+
+#include <x86/cpu.h>
+
+/** Memory area to use when passing data to BIOS interrupts (56KB).
+ * @note		Area is actually 60KB, but the last 4KB are used for
+ *			the stack. */
+#define BIOS_MEM_BASE		0x1000
+#define BIOS_MEM_SIZE		0xe000
+
+/** Convert a segment + offset pair to a linear address. */
+#define SEGOFF2LIN(segoff) \
+	(ptr_t)((((segoff) & 0xffff0000) >> 12) + ((segoff) & 0xffff))
+
+/** Convert a linear address to a segment + offset pair. */
+#define LIN2SEGOFF(lin) \
+	(uint32_t)(((lin & 0xfffffff0) << 12) + (lin & 0xf))
+
+/** Structure describing registers to pass to a BIOS interrupt. */
+typedef struct bios_regs {
+	union {
+		struct {
+			uint32_t eflags;
+			uint32_t eax;
+			uint32_t ebx;
+			uint32_t ecx;
+			uint32_t edx;
+			uint32_t edi;
+			uint32_t esi;
+			uint32_t ebp;
+			uint32_t _es;
+		};
+		struct {
+			uint16_t flags, _hflags;
+			uint16_t ax, _hax;
+			uint16_t bx, _hbx;
+			uint16_t cx, _hcx;
+			uint16_t dx, _hdx;
+			uint16_t di, _hdi;
+			uint16_t si, _hsi;
+			uint16_t bp, _hbp;
+			uint16_t es, _hes;
+		};
+	};
+} bios_regs_t;
+
+/** Initialise a BIOS registers structure.
+ * @param regs		Structure to initialise. */
+static inline void bios_regs_init(bios_regs_t *regs) {
+	memset(regs, 0, sizeof(bios_regs_t));
+}
+
+extern void bios_call(uint8_t num, bios_regs_t *regs);
 
 extern void bios_console_init(void);
 
 extern void platform_init(void);
 
-#endif /* __ASM__ */
 #endif /* __BIOS_BIOS_H */
