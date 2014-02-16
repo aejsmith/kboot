e419d2499331a37dc3e2ed4b54db169d4b72e021
Alex Smith <alex@alex-smith.me.uk>
2014-02-16 13:38:50 +0000

    Add generic physical memory allocation functions
    Add a set of generic physical memory allocation functions. These will be
    used by most platforms, except for EFI. When running under EFI, the
    firmware is in control of the memory map and it is possible for it to
    change underneath the loader, so for EFI the memory allocation functions
    will use the firmware interfaces to allocate memory. On other platforms
    the loader has full control of the memory map and allocates memory
    itself. Whether or not the generic functions are used is determined by
    the PLATFORM_HAS_MM flag, this is defined when the platform provides its
    own allocation functions (i.e. EFI).
    
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- source/loader/include/memory.h
+++ source/loader/include/memory.h
@@ -24,8 +24,50 @@
 
 #include <arch/page.h>
 
+#include <lib/list.h>
+
+/** Physical memory range descriptor. */
+typedef struct memory_range {
+	list_t header;			/**< Link to memory range list. */
+
+	phys_ptr_t start;		/**< Start of range. */
+	phys_size_t size;		/**< Size of range. */
+	uint8_t type;			/**< Type of the range. */
+} memory_range_t;
+
+/**
+ * Memory type definitions.
+ *
+ * Memory types to be used with the memory allocation functions. These match
+ * the types specified by the KBoot spec, with some additions.
+ */
+#define MEMORY_TYPE_FREE	0	/**< Free, usable memory. */
+#define MEMORY_TYPE_ALLOCATED	1	/**< Kernel image and other non-reclaimable data. */
+#define MEMORY_TYPE_RECLAIMABLE	2	/**< Memory reclaimable when boot information is no longer needed. */
+#define MEMORY_TYPE_PAGETABLES	3	/**< Temporary page tables for the kernel. */
+#define MEMORY_TYPE_STACK	4	/**< Stack set up for the kernel. */
+#define MEMORY_TYPE_MODULES	5	/**< Module data. */
+#define MEMORY_TYPE_INTERNAL	6	/**< Freed before the OS is entered. */
+
+/** Memory allocation behaviour flags. */
+#define MEMORY_ALLOC_HIGH	(1<<0)	/**< Allocate highest possible address. */
+
 extern void *malloc(size_t size);
 extern void *realloc(void *addr, size_t size);
 extern void free(void *addr);
+
+extern void *memory_alloc(phys_size_t size, phys_size_t align,
+	phys_ptr_t min_addr, phys_ptr_t max_addr, uint8_t type, unsigned flags,
+	phys_ptr_t *_phys);
+extern void memory_finalize(list_t *memory_map);
+extern void memory_dump(list_t *memory_map);
+
+#ifndef PLATFORM_HAS_MM
+
+extern void memory_add(phys_ptr_t start, phys_size_t size, uint8_t type);
+extern void memory_protect(phys_ptr_t start, phys_size_t size);
+extern void memory_init(void);
+
+#endif /* !PLATFORM_HAS_MM */
 
 #endif /* __MEMORY_H */
