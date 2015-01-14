8f05d21f0da5aa8c0970007319f5b747df06173c
Alex Smith <alex@alex-smith.me.uk>
2015-01-14 21:25:32 +0000

    Add memcmp() function
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- source/loader/lib/string.c
+++ source/loader/lib/string.c
@@ -102,6 +102,28 @@ void *memmove(void *dest, const void *src, size_t count) {
     return dest;
 }
 
+/** Compare 2 chunks of memory.
+ * @param p1            Pointer to the first chunk.
+ * @param p2            Pointer to the second chunk.
+ * @param count         Number of bytes to compare.
+ * @return              An integer less than, equal to or greater than 0 if
+ *                      p1 is found, respectively, to be less than, to match,
+ *                      or to be greater than p2. */
+int memcmp(const void *p1, const void *p2, size_t count) {
+    const unsigned char *s1 = (const unsigned char *)p1;
+    const unsigned char *s2 = (const unsigned char *)p2;
+
+    while (count--) {
+        if (*s1 != *s2)
+            return *s1 - *s2;
+
+        s1++;
+        s2++;
+    }
+
+    return 0;
+}
+
 /** Get the length of a string.
  * @param str           Pointer to the string.
  * @return              Length of the string. */
