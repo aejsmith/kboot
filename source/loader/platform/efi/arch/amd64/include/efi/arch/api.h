c97a44da4913bf9aa50919f87f8b59a9cadd4cee
Alex Smith <alex@alex-smith.me.uk>
2015-01-14 14:10:19 +0000

    EFI: amd64: Handle calling convention conversion in EFI call wrapper
    Clang does not currently correctly implement attribute((ms_abi)) - it
    does not reserve the register argument home space on the stack, resulting
    in occassional stack corruption during EFI calls.
    
    Workaround this by handling calling convention conversion ourself in the
    assembly EFI call wrapper. We now require a separate wrapper function
    depending on the number of arguments in order to correctly handle the
    conversion, so do some preprocessor magic/abuse to make the efi_call()
    macro select the correct wrapper based on its number of arguments.
    
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- source/loader/platform/efi/arch/amd64/include/efi/arch/api.h
+++ source/loader/platform/efi/arch/amd64/include/efi/arch/api.h
@@ -25,29 +25,47 @@
 /**
  * EFI calling convention attribute.
  *
- * On x86_64, EFI uses the Microsoft calling convention, therefore we must
- * mark all EFI APIs with the ms_abi attribute so that the right calling
- * convention is used.
+ * On x86_64, EFI uses the Microsoft calling convention. We could use the ms_abi
+ * attribute, however support for it in clang does not work right, so for now
+ * we use the normal calling convention and convert in a wrapper function.
  */
-#define __efiapi    __attribute__((ms_abi))
+#define __efiapi
+
+/* Mostly borrowed from here:
+ * http://stackoverflow.com/questions/11761703/overloading-macro-on-number-of-arguments */
+#define __VA_NARG(...) __VA_NARG_I(_0, ## __VA_ARGS__, __RSEQ_N())
+#define __VA_NARG_I(...) __VA_NARG_N(__VA_ARGS__)
+#define __VA_NARG_N(_0, _1, _2, _3, _4, _5, _6, _7, N, ...) N
+#define __RSEQ_N() 7, 6, 5, 4, 3, 2, 1, 0
+
+#define __efi_vcall_(n) __efi_call##n
+#define __efi_vcall(n) __efi_vcall_(n)
 
 /**
  * EFI call wrapper.
  *
- * We must wrap EFI calls to restore the firmware's GDT/IDT before calling, and
- * restore ours afterward. This is a slightly nasty hack to call functions via
- * a wrapper (in start.S), that keeps type safety and relies on the compiler to
- * put all arguments in the right place.
+ * We wrap EFI calls both to convert between calling conventions and to restore
+ * the firmware's GDT/IDT before calling, and restore ours afterward. This is a
+ * horrible bundle of preprocessor abuse that forwards EFI calls to the right
+ * wrapper for the number of arguments, while keeping type safety.
  */
-#define efi_call(func, args...) \
+#define efi_call(func, ...) \
     __extension__ \
     ({ \
-        typeof(func) __wrapper = (typeof(func))__efi_call; \
+        typeof(func) __wrapper = (typeof(func)) __efi_vcall(__VA_NARG(__VA_ARGS__)); \
         __efi_call_func = (void *)func; \
-        __wrapper(args); \
+        __wrapper(__VA_ARGS__); \
     })
 
 extern void *__efi_call_func;
-extern unsigned long __efi_call(void) __efiapi;
+
+extern uint64_t __efi_call0(void);
+extern uint64_t __efi_call1(uint64_t);
+extern uint64_t __efi_call2(uint64_t, uint64_t);
+extern uint64_t __efi_call3(uint64_t, uint64_t, uint64_t);
+extern uint64_t __efi_call4(uint64_t, uint64_t, uint64_t, uint64_t);
+extern uint64_t __efi_call5(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
+extern uint64_t __efi_call6(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
+extern uint64_t __efi_call7(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
 
 #endif /* __EFI_ARCH_API_H */
