d31d56dfa83fc9c193c2102b9340006f558c85ce
Alex Smith <alex@alex-smith.me.uk>
2015-02-27 16:18:32 +0000

    Add a textbox UI widget
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- source/ui.c
+++ source/ui.c
@@ -19,6 +19,8 @@
  * @brief               User interface.
  */
 
+#include <lib/ctype.h>
+#include <lib/string.h>
 #include <lib/utility.h>
 
 #include <assert.h>
@@ -52,6 +54,24 @@ typedef struct ui_checkbox {
     value_t *value;                     /**< Value modified by the checkbox. */
 } ui_checkbox_t;
 
+/** Structure representing a textbox. */
+typedef struct ui_textbox {
+    ui_entry_t entry;                   /**< Entry header. */
+
+    const char *label;                  /**< Label for the textbox. */
+    value_t *value;                     /**< Value modified by the textbox. */
+} ui_textbox_t;
+
+/** Structure for a textbox editor window. */
+typedef struct ui_textbox_editor {
+    ui_window_t window;
+
+    char *buf;                          /**< String being edited. */
+    size_t len;                         /**< Current string length. */
+    size_t offset;                      /**< Current string offset. */
+    bool update;                        /**< Whether to update textbox when the window closes. */
+} ui_textbox_editor_t;
+
 /** Properties of the UI console. */
 static uint16_t ui_console_width;
 static uint16_t ui_console_height;
@@ -60,8 +80,11 @@ static uint16_t ui_console_height;
 console_t *ui_console;
 
 /** Dimensions of the content area. */
-#define UI_CONTENT_WIDTH    (ui_console_width - 4)
-#define UI_CONTENT_HEIGHT   (ui_console_height - 6)
+#define CONTENT_WIDTH               (ui_console_width - 4)
+#define CONTENT_HEIGHT              (ui_console_height - 6)
+
+/** Size of a chunk to allocate in the textbox editor window. */
+#define TEXTBOX_EDITOR_CHUNK_SIZE   128
 
 /** Print an action (for help text).
  * @param key           Key for the action.
@@ -137,8 +160,8 @@ static inline void set_content_region(void) {
 
     region.x = 2;
     region.y = 3;
-    region.width = UI_CONTENT_WIDTH;
-    region.height = UI_CONTENT_HEIGHT;
+    region.width = CONTENT_WIDTH;
+    region.height = CONTENT_HEIGHT;
     region.scrollable = false;
 
     console_set_region(ui_console, &region);
@@ -256,6 +279,9 @@ void ui_display(ui_window_t *window, console_t *console, unsigned timeout) {
 static void ui_list_destroy(ui_window_t *window) {
     ui_list_t *list = (ui_list_t *)window;
 
+    for (size_t i = 0; i < list->count; i++)
+        ui_entry_destroy(list->entries[i]);
+
     free(list->entries);
 }
 
@@ -296,7 +322,7 @@ static void ui_list_render(ui_window_t *window) {
     size_t end;
 
     /* Calculate the range of entries to display. */
-    end = min(list->offset + UI_CONTENT_HEIGHT, list->count);
+    end = min(list->offset + CONTENT_HEIGHT, list->count);
 
     /* Render the entries. */
     for (size_t i = list->offset; i < end; i++) {
@@ -364,7 +390,7 @@ static input_result_t ui_list_input(ui_window_t *window, uint16_t key) {
 
         /* If selected is now off screen, must scroll down. */
         list->selected++;
-        if (list->selected >= list->offset + UI_CONTENT_HEIGHT) {
+        if (list->selected >= list->offset + CONTENT_HEIGHT) {
             list->offset++;
             console_scroll_down(ui_console);
         }
@@ -432,8 +458,8 @@ void ui_list_insert(ui_window_t *window, ui_entry_t *entry, bool selected) {
 
     if (selected) {
         list->selected = pos;
-        if (pos >= UI_CONTENT_HEIGHT)
-            list->offset = (pos - UI_CONTENT_HEIGHT) + 1;
+        if (pos >= CONTENT_HEIGHT)
+            list->offset = (pos - CONTENT_HEIGHT) + 1;
     }
 }
 
@@ -505,13 +531,15 @@ ui_entry_t *ui_entry_create(const char *label, value_t *value) {
     switch (value->type) {
     case VALUE_TYPE_BOOLEAN:
         return ui_checkbox_create(label, value);
+    case VALUE_TYPE_STRING:
+        return ui_textbox_create(label, value);
     default:
         assert(0 && "Unhandled value type");
         return NULL;
     }
 }
 
-/** Render a check box.
+/** Render a checkbox.
  * @param entry         Entry to render. */
 static void ui_checkbox_render(ui_entry_t *entry) {
     ui_checkbox_t *box = (ui_checkbox_t *)entry;
@@ -544,7 +572,7 @@ static input_result_t ui_checkbox_input(ui_entry_t *entry, uint16_t key) {
     }
 }
 
-/** Check box entry type. */
+/** Checkbox entry type. */
 static ui_entry_type_t ui_checkbox_entry_type = {
     .render = ui_checkbox_render,
     .help = ui_checkbox_help,
@@ -562,6 +590,252 @@ ui_entry_t *ui_checkbox_create(const char *label, value_t *value) {
 
     box = malloc(sizeof(*box));
     box->entry.type = &ui_checkbox_entry_type;
+    box->label = label;
+    box->value = value;
+    return &box->entry;
+}
+
+/** Render a textbox editor window.
+ * @param window        Window to render. */
+static void ui_textbox_editor_render(ui_window_t *window) {
+    ui_textbox_editor_t *editor = (ui_textbox_editor_t *)window;
+    uint16_t x, y;
+
+    for (size_t i = 0; i <= editor->len; i++) {
+        if (i == editor->offset)
+            console_get_cursor(ui_console, &x, &y, NULL);
+
+        if (i < editor->len)
+            console_putc(ui_console, editor->buf[i]);
+    }
+
+    console_set_cursor(ui_console, x, y, true);
+}
+
+/** Write the help text for a textbox editor window.
+ * @param window        Window to write for. */
+static void ui_textbox_editor_help(ui_window_t *window) {
+    ui_print_action('\n', "Update");
+    ui_print_action('\e', "Cancel");
+}
+
+/** Insert a character to the buffer at the current position.
+ * @param editor        Editor being operated on.
+ * @param ch            Character to insert. */
+static void insert_textbox_char(ui_textbox_editor_t *editor, char ch) {
+    /* Resize the buffer if this will go over a chunk boundary. We allocate in
+     * chunks to avoid reallocating on every modification. */
+    if (!(editor->len % TEXTBOX_EDITOR_CHUNK_SIZE))
+        editor->buf = realloc(editor->buf, editor->len + TEXTBOX_EDITOR_CHUNK_SIZE);
+
+    console_putc(ui_console, ch);
+
+    if (editor->offset == editor->len) {
+        editor->buf[editor->len++] = ch;
+        editor->offset++;
+    } else {
+        size_t i;
+
+        memmove(&editor->buf[editor->offset + 1], &editor->buf[editor->offset], editor->len - editor->offset);
+        editor->buf[editor->offset++] = ch;
+        editor->len++;
+
+        /* Reprint the character plus everything after, maintaining the
+         * current cursor position. */
+        for (i = 0; i < editor->len - editor->offset; i++)
+            console_putc(ui_console, editor->buf[editor->offset + i]);
+        while (i--)
+            console_putc(ui_console, '\b');
+    }
+}
+
+/** Erase a character from the current position.
+ * @param editor        Editor being operated on.
+ * @param forward       If true, will erase the character at the current cursor
+ *                      position, else will erase the previous one. */
+static void erase_textbox_char(ui_textbox_editor_t *editor, bool forward) {
+    size_t i;
+
+    if (forward) {
+        if (editor->offset == editor->len)
+            return;
+    } else {
+        if (!editor->offset) {
+            return;
+        } else {
+            /* Decrement position and fall through. */
+            editor->offset--;
+            console_putc(ui_console, '\b');
+        }
+    }
+
+    editor->len--;
+    memmove(&editor->buf[editor->offset], &editor->buf[editor->offset + 1], editor->len - editor->offset);
+
+    /* If we're now on a chunk boundary, we can resize the buffer down a chunk. */
+    if (!(editor->len % TEXTBOX_EDITOR_CHUNK_SIZE))
+        editor->buf = realloc(editor->buf, editor->len);
+
+    /* Reprint everything, maintaining cursor position. */
+    for (i = 0; i < editor->len - editor->offset; i++)
+        console_putc(ui_console, editor->buf[editor->offset + i]);
+    console_putc(ui_console, ' ');
+    i++;
+    while (i--)
+        console_putc(ui_console, '\b');
+}
+
+/** Handle input on a textbox editor window.
+ * @param window        Window input was performed on.
+ * @param key           Key that was pressed.
+ * @return              Input handling result. */
+static input_result_t ui_textbox_editor_input(ui_window_t *window, uint16_t key) {
+    ui_textbox_editor_t *editor = (ui_textbox_editor_t *)window;
+
+    /* TODO: Common line editing code, this is all pretty much the same as the
+     * shell's editing code. */
+    switch (key) {
+    case '\n':
+        editor->update = true;
+    case '\e':
+        return INPUT_CLOSE;
+    case CONSOLE_KEY_LEFT:
+        if (editor->offset) {
+            console_putc(ui_console, '\b');
+            editor->offset--;
+        }
+
+        break;
+    case CONSOLE_KEY_RIGHT:
+        if (editor->offset != editor->len) {
+            console_putc(ui_console, editor->buf[editor->offset]);
+            editor->offset++;
+        }
+
+        break;
+    case CONSOLE_KEY_HOME:
+        while (editor->offset) {
+            console_putc(ui_console, '\b');
+            editor->offset--;
+        }
+
+        break;
+    case CONSOLE_KEY_END:
+        while (editor->offset < editor->len) {
+            console_putc(ui_console, editor->buf[editor->offset]);
+            editor->offset++;
+        }
+
+        break;
+    case '\b':
+        erase_textbox_char(editor, false);
+        break;
+    case 0x7f:
+        erase_textbox_char(editor, true);
+        break;
+    default:
+        if (isprint(key))
+            insert_textbox_char(editor, key);
+
+        break;
+    }
+
+    return INPUT_HANDLED;
+}
+
+/** Text box editor window type. */
+static ui_window_type_t ui_textbox_editor_window_type = {
+    .render = ui_textbox_editor_render,
+    .help = ui_textbox_editor_help,
+    .input = ui_textbox_editor_input,
+};
+
+/** Render a textbox.
+ * @param entry         Entry to render. */
+static void ui_textbox_render(ui_entry_t *entry) {
+    ui_textbox_t *box = (ui_textbox_t *)entry;
+    size_t len, avail;
+
+    ui_printf("%s", box->label);
+
+    /* Work out the length available to put the string value in. */
+    avail = CONTENT_WIDTH - strlen(box->label) - 3;
+    len = strlen(box->value->string);
+    if (len > avail) {
+        ui_printf(" [");
+        for (size_t i = 0; i < avail - 3; i++)
+            console_putc(ui_console, box->value->string[i]);
+        ui_printf("...]");
+    } else {
+        console_set_cursor(ui_console, 0 - len - 2, 0, false);
+        ui_printf("[%s]", box->value->string);
+    }
+}
+
+/** Write the help text for a textbox.
+ * @param entry         Entry to write for. */
+static void ui_textbox_help(ui_entry_t *entry) {
+    ui_print_action('\n', "Edit");
+}
+
+/** Handle input on a textbox.
+ * @param entry         Entry input was performed on.
+ * @param key           Key that was pressed.
+ * @return              Input handling result. */
+static input_result_t ui_textbox_input(ui_entry_t *entry, uint16_t key) {
+    ui_textbox_t *box = (ui_textbox_t *)entry;
+
+    if (key == '\n') {
+        ui_textbox_editor_t editor;
+        size_t len;
+        char *title __cleanup_free;
+
+        /* Determine the window title. */
+        len = strlen(box->label) + 8;
+        title = malloc(len);
+        snprintf(title, len, "Edit '%s'", box->label);
+
+        editor.window.type = &ui_textbox_editor_window_type;
+        editor.window.title = title;
+        editor.len = strlen(box->value->string);
+        editor.offset = editor.len;
+        editor.buf = malloc(round_up(editor.len, TEXTBOX_EDITOR_CHUNK_SIZE));
+        memcpy(editor.buf, box->value->string, editor.len);
+        editor.update = false;
+
+        ui_display(&editor.window, ui_console, 0);
+
+        if (editor.update) {
+            /* Resize buffer down to exact size. */
+            free(box->value->string);
+            box->value->string = realloc(editor.buf, editor.len + 1);
+            box->value->string[editor.len] = 0;
+        }
+
+        return INPUT_RENDER_WINDOW;
+    } else {
+        return INPUT_HANDLED;
+    }
+}
+
+/** Textbox entry type. */
+static ui_entry_type_t ui_textbox_entry_type = {
+    .render = ui_textbox_render,
+    .help = ui_textbox_help,
+    .input = ui_textbox_input,
+};
+
+/** Create a textbox entry.
+ * @param label         Label for the textbox.
+ * @param value         Value to store state in (should be VALUE_TYPE_STRING).
+ * @return              Pointer to created entry. */
+ui_entry_t *ui_textbox_create(const char *label, value_t *value) {
+    ui_textbox_t *box;
+
+    assert(value->type == VALUE_TYPE_STRING);
+
+    box = malloc(sizeof(*box));
+    box->entry.type = &ui_textbox_entry_type;
     box->label = label;
     box->value = value;
     return &box->entry;
