7f5450e212d5707c85027dd5ae8d0db0101689fb
Alex Smith <alex@alex-smith.me.uk>
2021-02-14 14:15:58 +0000

    Add configurations for individual ARM64 platforms
    The dt-arm64 configuration will support all DT ARM64 platforms, however there
    are benefits to having configurations for individual platforms as well:
    
     - Can build a smaller loader if support for only one platform is desired.
     - Allows us to make assumptions about the hardware - this will be used to
       hardcode UART configuration when building for a specific platform, which
       will be useful for debugging before the device tree is parsed to find the
       UART.

--- test/qemu/dt-arm64.sh
+++ test/qemu/dt-arm64.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
 
-builddir=build/dt-arm64
+platform=$(basename "$0" ".sh")
+builddir="build/${platform}"
 
 qemu-system-aarch64 -M virt -cpu cortex-a53 -serial stdio -m 512 -kernel "${builddir}/bin/kboot.bin"
